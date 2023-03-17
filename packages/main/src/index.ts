17eec80ddad4aac4135c600c6981b8ff7e665073
Gonçalo Mendes <goncalo.mendes@izertis.com>
2023-03-17 07:40:11 +0000

    first commit

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -38,6 +38,7 @@ const createWindow = async () => {
     show: false, // Use 'ready-to-show' event to show window
     vibrancy: 'under-window',
     visualEffectState: 'active',
+    autoHideMenuBar: true,
     webPreferences: {
       preload: join(__dirname, '../../preload/dist/index.cjs'),
       contextIsolation: env.MODE !== 'test',   // Spectron tests can't work with contextIsolation: true
